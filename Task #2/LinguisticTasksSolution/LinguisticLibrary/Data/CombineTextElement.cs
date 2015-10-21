f6746299b520a5058f9ed625b68580966d8e82e1
George <Georgebushington75@gmail.com>
2015-10-21 20:25:45 +0200

    Abstract class of text containers.
    Abstract class  which is first point for making containers for text
    elements.

---
+++ Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/CombineTextElement.cs
@@ -0,0 +1,85 @@
+﻿using LinguisticLibrary.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinguisticLibrary.Data
+{
+    abstract class CombineTextElement<T> : TextElement, ICollection<T> where T : ITextElement
+    {
+        private ICollection<T> _items;
+
+        public CombineTextElement()
+        {
+            _items = new List<T>();
+        }
+
+        public override string TextValue
+        {
+            get
+            {
+                return _items.Aggregate(string.Empty, (value, item) => string.Concat(value, item.TextValue));
+            }
+        }
+
+
+        #region ICollection interface implementation
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
