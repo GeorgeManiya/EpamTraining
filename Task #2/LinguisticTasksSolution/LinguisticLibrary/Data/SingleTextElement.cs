8008eebab9d07de669b81db5939a26f50c1f3e1e
George <Georgebushington75@gmail.com>
2015-10-22 14:30:26 +0200

    Abstract class that provides ability to create single text elements.

---
+++ Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/SingleTextElement.cs
@@ -0,0 +1,13 @@
+﻿using LinguisticLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinguisticLibrary.Data
+{
+    internal abstract class SingleTextElement : TextElement, ISingleTextElement
+    {
+    }
+}
