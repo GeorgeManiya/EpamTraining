772a553bc16bd6b0fa46b9d4b291f0fbd6a90f0f
George <Georgebushington75@gmail.com>
2015-10-23 11:28:55 +0200

    Allows sentence contains just single text elements.

--- Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/Sentence.cs
+++ Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/Sentence.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinguisticLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LinguisticLibrary.Data
 {
-    class Sentence : CombineTextElement<Word>
+    class Sentence : CombineTextElement<ISingleTextElement>
     {
     }
 }
