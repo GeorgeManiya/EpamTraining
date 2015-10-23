3cbe4f450f1da5d0a66586c609a827d03a37d84a
George <Georgebushington75@gmail.com>
2015-10-23 11:29:24 +0200

    Adding default punctuation marks.

--- Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/PunctuationMark.cs
+++ Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/PunctuationMark.cs
@@ -1,18 +1,20 @@
 ﻿using LinguisticLibrary.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinguisticLibrary.Data
 {
-    enum PunctuationMarkType
+    public enum PunctuationMarkType
     {
         EndMark,                // dot in the end of sentence
         CommaMark,              // default comma
         QuestionMark,           // question in the end of sentence
         ExclamationMark,        // exclamation in the end of sentence
+        AccentMark,             // exclamation with question
         AbreviationMark,        // dot, that hide some part of the text
         UnfinishedMark,         // three points
         EnumerationMark,        // dot with comma
@@ -42,6 +44,52 @@ namespace LinguisticLibrary.Data
         }
     }
 
+    static class DefaultPunctuationMarks
+    {
+        private static ICollection<PunctuationMark> _terminalPunctuationMarks;
+        public static ICollection<PunctuationMark> TerminalPunctuationMarks
+        {
+            get { return _terminalPunctuationMarks; }
+        }
+
+        private static ICollection<PunctuationMark> _internalPunctuationMarks;
+        public static ICollection<PunctuationMark> InternalPunctuationMarks
+        {
+            get { return _internalPunctuationMarks; }
+        }
+
+        private static PunctuationMark _spaceMark;
+        public static PunctuationMark SpaceMark
+        {
+            get { return _spaceMark; }
+        }
+
+        static DefaultPunctuationMarks()
+        {
+            _spaceMark = new PunctuationMark(" ", PunctuationMarkType.SpaceMark);
+
+            _terminalPunctuationMarks = new Collection<PunctuationMark>()
+            {
+                new PunctuationMark(".", PunctuationMarkType.EndMark),
+                new PunctuationMark("?", PunctuationMarkType.QuestionMark),
+                new PunctuationMark("!", PunctuationMarkType.ExclamationMark),
+                new PunctuationMark("...", PunctuationMarkType.UnfinishedMark),
+                new PunctuationMark("?!", PunctuationMarkType.AccentMark),
+                new PunctuationMark("!?", PunctuationMarkType.AccentMark)
+            };
+
+            _internalPunctuationMarks = new Collection<PunctuationMark>()
+            {
+                new PunctuationMark(",", PunctuationMarkType.CommaMark),
+                new PunctuationMark(".", PunctuationMarkType.AbreviationMark),
+                new PunctuationMark(";", PunctuationMarkType.EnumerationMark),
+                new PunctuationMark(":", PunctuationMarkType.GeneralizationMark),
+                new PunctuationMark("-", PunctuationMarkType.ConnectionMark),
+                new PunctuationMark("-", PunctuationMarkType.IllustrationMark),
+                new PunctuationMark("(", PunctuationMarkType.AdditionalMark),
+                new PunctuationMark(")", PunctuationMarkType.AdditionalMark),
+                new PunctuationMark("'", PunctuationMarkType.CitationMark)
+            };
         }
     }
 }
