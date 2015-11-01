39e6b97fb4d0b5bfcc117a9ad4d1be62286c91b4
George <Georgebushington75@gmail.com>
2015-11-01 21:48:21 +0200

    IValueConverter

---
+++ Task #2/LinguisticTasksSolution/LinguisticFeaturesAnalyzer/Data/BooleanToBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LinguisticFeaturesAnalyzer.Data
+{
+    class BooleanToBrushConverter : IValueConverter
+    {
+        public Brush True { get; set; }
+        public Brush False { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val;
+            Boolean.TryParse(value.ToString(), out val);
+            return val ? True : False;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
