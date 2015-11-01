6880a11b89a705811e1034d1f5dec2ff2adbe291
George <Georgebushington75@gmail.com>
2015-11-01 21:50:09 +0200

    Text element, that can contains some words.

---
+++ Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/CompositeWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinguisticLibrary.Interfaces;
+
+namespace LinguisticLibrary.Data
+{
+    public class CompositeWord : CombineTextElement<ISingleTextElement>, IWord
+    {
+        public SingleTextElementInnerOption InnerOption
+        {
+            get
+            {
+                return this.Any() ? this.First().InnerOption : SingleTextElementInnerOption.None;
+            }
+            set
+            {
+                if (this.Any())
+                {
+                    this.First().InnerOption = value;
+                }
+            }
+        }
+    }
+}
