cab83286384d34d4f076d301349ded564a31e825
George <Georgebushington75@gmail.com>
2015-11-02 14:59:32 +0200

    Final application.

--- Task #2/LinguisticTasksSolution/LinguisticFeaturesAnalyzer/MainWindow.xaml.cs
+++ Task #2/LinguisticTasksSolution/LinguisticFeaturesAnalyzer/MainWindow.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 using LinguisticLibrary.Data;
-using LinguisticLibrary.Interfaces;
 using LinguisticFeaturesAnalyzer.Data;
+using LinguisticLibrary.Interfaces;
 
 namespace LinguisticFeaturesAnalyzer
 {
@@ -25,12 +15,14 @@ namespace LinguisticFeaturesAnalyzer
     public partial class MainWindow : Window
     {
         private readonly OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+        private Text _text;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            SentenceTypeComboBox.ItemsSource = new List<PunctuationMarkType>() { PunctuationMarkType.QuestionMark, PunctuationMarkType.ExclamationMark };
+            TerminalPunctuationTypeComboBox.ItemsSource = DefaultPunctuationMarks.TerminalPunctuationMarks;
+            InternalPunctuationTypeComboBox.ItemsSource = DefaultPunctuationMarks.InternalPunctuationMarks;
 
             myOpenFileDialog.FileOk += OnOpenFileDialogOK;
         }
@@ -49,24 +41,90 @@ namespace LinguisticFeaturesAnalyzer
         void DisplayFile(string filePath)
         {
             var text = TextParser.Parse(filePath);
-
-            var activeSentences = new List<ActiveSentence>();
-            foreach (var sentence in text)
-            {
-                activeSentences.Add(ActiveTextComponentFactory.GetActivatedSentence(sentence));
-            }
-            TextContainer.ItemsSource = activeSentences;
+            _text = ActiveTextComponentFactory.GetActivatedText(text);
+            TextContainer.ItemsSource = _text;
         }
 
         private void OnFilterBySentenceTypeButtonClick(object sender, RoutedEventArgs e)
         {
-            if (SentenceTypeComboBox.SelectedItem == null || TextContainer.ItemsSource == null) return;
+            if (TextContainer.ItemsSource == null) return;
 
-            var sentenceType = (PunctuationMarkType)SentenceTypeComboBox.SelectedItem;
-            var filteredSentences = ((List<ActiveSentence>)TextContainer.ItemsSource).Where(sentence => sentence.Type == sentenceType);
-            foreach(var sentence in filteredSentences)
+            foreach (ActiveSentence activeSentence in _text.GetSentences())
             {
-                sentence.IsActive = true;
+                activeSentence.Deactivate();
+            }
+
+            var type = EnableByTerminalPunctuationsRadioButton.IsChecked.Value
+                ? ((PunctuationMark)TerminalPunctuationTypeComboBox.SelectedItem).Type
+                : ((PunctuationMark)InternalPunctuationTypeComboBox.SelectedItem).Type;
+
+            if (EnableByTerminalPunctuationsRadioButton.IsChecked.Value)
+            {
+                if (TerminalPunctuationTypeComboBox.SelectedItem == null) return;
+
+                var filteredSentences = _text.GetSentences().
+                    OfType<ActiveSentence>().Where(sentence => sentence.Type == type);
+                foreach (var sentence in filteredSentences)
+                {
+                    sentence.IsActive = true;
+                }
+            }
+
+            if (EnableByInternalPunctuationsRadioButton.IsChecked.Value)
+            {
+                if (InternalPunctuationTypeComboBox.SelectedItem == null) return;
+
+                var filteredSentences = _text.GetSentences().
+                    OfType<ActiveSentence>().Where(sentence => sentence.OfType<PunctuationMark>().Any(mark => mark.Type == type));
+                foreach (var sentence in filteredSentences)
+                {
+                    sentence.IsActive = true;
+                }
+            }
+        }
+
+        private void OnChangeWordsInTextButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (TextContainer.ItemsSource == null) return;
+
+            if (string.IsNullOrEmpty(OriginalWord.Text) || string.IsNullOrEmpty(NewWord.Text)) return;
+
+            foreach (var sentence in _text.GetSentences())
+            {
+                foreach (var word in sentence.OfType<IWord>().Where(w => w.StringValue.Trim() == OriginalWord.Text))
+                    word.StringValue = NewWord.Text;
+            }
+
+            TextContainer.Items.Refresh();
+        }
+
+        private void OnSortByButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (SortByWordsCountRadioButton.IsChecked.Value)
+            {
+                var sortedSentences = _text.GetSentences().OrderBy(sentence => sentence.Count);
+                var text = new Text();
+                
+                foreach(var sentence in sortedSentences)
+                {
+                    var paragraph = new Paragraph();
+                    paragraph.Add(sentence);
+                    text.Add(paragraph);
+                }
+                
+                TextContainer.ItemsSource = text;
+            }
+            else if (SortBySentenceCountRadioButton.IsChecked.Value)
+            {
+                var sortedParagraphs = _text.OrderBy(paragraph => paragraph.Count);
+                var text = new Text();
+                foreach(var paragraph in sortedParagraphs)
+                {
+                    text.Add(paragraph);
+                    text.Add(new Paragraph());
+                }
+
+                TextContainer.ItemsSource = text;
             }
         }
     }
