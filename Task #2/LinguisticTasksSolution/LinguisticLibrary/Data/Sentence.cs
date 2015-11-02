538771d8318fdcc8643147f891d5fa3127f5e976
George <Georgebushington75@gmail.com>
2015-11-02 15:00:59 +0200

    Concat function.

--- Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/Sentence.cs
+++ Task #2/LinguisticTasksSolution/LinguisticLibrary/Data/Sentence.cs
@@ -33,7 +33,34 @@ namespace LinguisticLibrary.Data
 
         public void Concat(string value)
         {
+            if (!IsNotFinished)
+            {
+                var endMark = (PunctuationMark)this.Last();
+                this.Remove(endMark);
 
+                var sentencePart = TextParser.ParseSentenceString(value, new PunctuationMark());
+                foreach(var textElement in sentencePart)
+                {
+                    this.Add(textElement);
+                }
+
+                this.Add(endMark);
+            }
+            else
+            {
+                var terminalPunctuationMark = value.GetFirstOrDefaultPunctuationMark(DefaultPunctuationMarks.TerminalPunctuationMarks);
+                if (terminalPunctuationMark.HasValue)
+                {
+                    var index = value.IndexOfPunctuationMark(terminalPunctuationMark);
+                    value = value.Substring(0, index);
+                }
+
+                var sentencePart = TextParser.ParseSentenceString(value, terminalPunctuationMark);
+                foreach (var textElement in sentencePart)
+                {
+                    this.Add(textElement);
+                }
+            }
         }
     }
 }
