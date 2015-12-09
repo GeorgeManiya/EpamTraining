66be3d8bd750c4e132073db35debaa65d0246da9
George <Georgebushington75@gmail.com>
2015-12-09 22:26:34 +0300

    Basic solution files.

---
+++ Task #5/SalesInfoSolution/SalesInfoMVCApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesInfoMVCApplication.Startup))]
+namespace SalesInfoMVCApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
